8b8d4d18ca3e89c550ffc7f2810eff4e4e29029c
bmontilorusso <b.montilorusso@gmail.com>
2025-06-21 20:57:45 -0300

    Comienzo de Alta de Usuarios.

--- php/alta_usuario.php
+++ php/alta_usuario.php
@@ -2,6 +2,27 @@
 
 // ALTA DE USUARIOS:
 
+// Importaciones:
+include('conexion_DB.php');
+
+// Declaración de variables:
+
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$mail = $_POST['mail'];
+$nuevoUsuario = $_POST['nuevoUsuario'];
+$nuevaPass = $_POST['nuevaPass'];
+
+// Generación de la Query:
+$sql = "INSERT INTO USUARIOS (NOMBRE, APELLIDO, MAIL, USUARIO, PASS, ID_CARGO, HABILITADO) VALUES ('$nombre', '$apellido', '$mail', '$nuevoUsuario', '$nuevaPass', 1, 'SI');";
+
+// Resultado:
+$resultado = mysqli_query($conn, $sql);
+
+// Validación de éxito en creación:
+
+
+
 
 
 
