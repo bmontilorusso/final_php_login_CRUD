a5a5a27d697b93b5b199b8cae8af2447bfef4b95
bmontilorusso <b.montilorusso@gmail.com>
2025-06-21 04:56:35 -0300

    Ajustes varios

--- js/script.js
+++ js/script.js
@@ -2,19 +2,19 @@
 
 // CAMBIO DE FORMULARIO (Login - AltaUsuario):
 
-var mostrarAltaUsuario = document.getElementById("aqui");
-var mostrarLogIn = document.getElementById("volver");
-var formAltaUsuario = document.getElementById("formAltaUsuario");
-var formLogIn = document.getElementById("formLogin");
+var mostrarAltaUsuario = document.getElementById('aqui');
+var mostrarLogIn = document.getElementById('volver');
+var formAltaUsuario = document.getElementById('formAltaUsuario');
+var formLogIn = document.getElementById('formLogin');
 
 
-mostrarAltaUsuario.addEventListener("click", function() {
+mostrarAltaUsuario.addEventListener('click', function() {
     formLogIn.style.display = 'none';
     formAltaUsuario.style.display = 'block';
 });
 
 
-mostrarLogIn.addEventListener("click", function() {
+mostrarLogIn.addEventListener('click', function() {
     formLogIn.style.display = 'block';
     formAltaUsuario.style.display = 'none';
 });
