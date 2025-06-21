48f0727fd27667982c03f1b3dab4534660bc202e
bmontilorusso <b.montilorusso@gmail.com>
2025-06-21 04:34:34 -0300

    Comienzo parte JavaScript.

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Trabajo FINAL PHP - Monti Bruno & Reale Aldana 
+
+// CAMBIO DE FORMULARIO (Login - AltaUsuario):
+
+var mostrarAltaUsuario = document.getElementById('aqui');
+var mostrarLogIn = document.getElementById('aqui');
+var mostrarLogIn = document.getElementById('aqui');
+var mostrarLogIn = document.getElementById('aqui');
+
+
+mostrarLogIn.addEventListener("Click", function() {
+    form-login
+})
